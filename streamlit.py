27b8a22fc003728b78c91c92aa773fb8dcd14c4f
Herrem03 <53403380+Herrem03@users.noreply.github.com>
2020-11-26 16:37:35 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -16,26 +16,21 @@ st.sidebar.text('2020 v1.0.0')
 
 if page == "Accueil":
     st.header('Avant-propos')
-    st.markdown(
-        "Cet outil permet de simuler différents projets d'investissement immobilier à partir d'offres leboncoin.fr et seloger.com: ")
+    st.markdown("Cet outil permet de simuler différents projets d'investissement immobilier à partir d'offres leboncoin.fr et seloger.com: ")
     st.markdown("- SCI ")
     st.markdown("- LMNP ")
     st.markdown("Le quartier est déterminé à partir de l'analyse sémantique de la description")
 
 if page == "Simulation":
     st.subheader('Bien à analyser')
-    choice = st.radio('Choisissez le mode de saisie', ['A la mano', "A partir de l'URL"])
-    if choice == 'A la mano':
-        st.write('Non disponible')
-    if choice == "A partir de l'URL":
-        url = st.text_input("Collez ici l'adresse url de l'annonce")
-        st.markdown(':warning: Vérifiez la validité des informations extraites')
-        @st.cache(allow_output_mutation=True)
-        if st.button("Sauvegarder annonce"):
-            get_data().append({"URL ": url})
+    url = st.text_input("Collez ici l'adresse url de l'annonce")
+    st.markdown(':warning: Vérifiez la validité des informations extraites')
+    @st.cache(allow_output_mutation=True)
+    if st.button("Sauvegarder annonce"):
+        get_data().append({"URL ": url})
 
-        annonce = pd.DataFrame(get_data())
-        st.write(annonce)
+    annonce = pd.DataFrame(get_data())
+    st.write(annonce)
 
     st.subheader('Paramètres simulation')
     st.selectbox('Statut Fiscal', ['SCI', 'LMNP', 'SCCV'])

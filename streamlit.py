59982e5d44fb59d4c054cb6bc98326a496cb900e
Herrem03 <53403380+Herrem03@users.noreply.github.com>
2020-11-26 15:05:04 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -28,12 +28,8 @@ if page == "Simulation":
     def get_data():
         return []
 
-    user_id = st.text_input("User ID")
-    foo = st.slider("foo", 0, 100)
-    bar = st.slider("bar", 0, 100)
-
-    if st.button("Add row"):
-        get_data().append({"UserID": user_id, "foo": foo, "bar": bar})
+    if st.button("Sauvegarder annonce"):
+        get_data().append({"URL ": url})
 
     st.write(pd.DataFrame(get_data()))
 

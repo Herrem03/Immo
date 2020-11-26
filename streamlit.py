62f0360a4e1cfd9518554dc84708dd040e64a3cc
Herrem03 <53403380+Herrem03@users.noreply.github.com>
2020-11-26 15:57:04 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,12 +1,14 @@
-#Packages
+# Packages
 import streamlit as st
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
-#Fonctions
+
+# Fonctions
 def get_data():
     return []
+
 
 def scrap(url):
     page = requests.get(url)
@@ -17,15 +19,16 @@ def scrap(url):
     return surface
 
 
-#--------------Application Streamlit-------------#
-#Sidebar
+# --------------Application Streamlit-------------#
+# Sidebar
 st.sidebar.title("Analyse projet immobilier")
 page = st.sidebar.selectbox("Menu", ["Accueil", "Analyse macro", "Simulation", "A propos"])
 st.sidebar.text('2020 v1.0.0')
 
 if page == "Accueil":
     st.header('Avant-propos')
-    st.markdown("Cet outil permet de simuler différents projets d'investissement immobilier à partir d'offres leboncoin.fr et seloger.com: ")
+    st.markdown(
+        "Cet outil permet de simuler différents projets d'investissement immobilier à partir d'offres leboncoin.fr et seloger.com: ")
     st.markdown("- SCI ")
     st.markdown("- LMNP ")
     st.markdown("Le quartier est déterminé à partir de l'analyse sémantique de la description")
@@ -39,15 +42,13 @@ if page == "Simulation":
         url = st.text_input("Collez ici l'adresse url de l'annonce")
         st.markdown(':warning: Vérifiez la validité des informations extraites')
         @st.cache(allow_output_mutation=True)
-
         if st.button("Sauvegarder annonce"):
             get_data().append({"URL ": url})
-        
+
         annonce = pd.DataFrame(get_data())
         st.write(annonce)
-        st.write('surface', scrap(url))
-        
-        
+        st.write(scrap(url))
+
     st.subheader('Paramètres simulation')
     st.selectbox('Statut Fiscal', ['SCI', 'LMNP', 'SCCV'])
     st.slider("Nombre d'investisseurs", min_value=1, max_value=6, value=5, step=1)
